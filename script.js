f2c829d385b1465d647e966a1fc76fb7b6401f13
lakshyasingh188 <lakshyasinghe.c.e@gmail.com>
2025-12-22 19:26:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const heroImages = [
+    "heroine1.avif",
+    "heroine2.jpg",
+    "heroine3.jpg",
+    "forenssic4.webp"
+];
+
+let index = 0;
+const hero = document.querySelector(".hero");
+
+setInterval(() => {
+    index = (index + 1) % heroImages.length;
+    hero.style.backgroundImage = `url('${heroImages[index]}')`;
+}, 3000);
