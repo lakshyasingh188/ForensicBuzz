a5367c69862bfdc43c4b94ae5bf2909d1f34280c
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-01-24 15:02:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,17 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
-// YOUR FIREBASE CONFIG (UNCHANGED)
 const firebaseConfig = {
-  apiKey: "AIzaSyA5-LsR0dMDpyZsuXhygC6tDPk5RzGiGZ8",
-  authDomain: "forensicbuzz-802d5.firebaseapp.com",
-  projectId: "forensicbuzz-802d5",
-  storageBucket: "forensicbuzz-802d5.firebasestorage.app",
-  messagingSenderId: "110785737752",
-  appId: "1:110785737752:web:8e180af63c933ad88c7f6d",
-  measurementId: "G-0633PY6W7Q"
+  apiKey: "AIzaSyBOZiAJJ8CvY8ScX4SjWk64jrvULN2LZA",
+  authDomain: "forensicbuzz1.firebaseapp.com",
+  projectId: "forensicbuzz1",
+  storageBucket: "forensicbuzz1.firebasestorage.app",
+  messagingSenderId: "557090337648",
+  appId: "1:557090337648:web:147fddd95747880a5415b5"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
