6448052d730bef4a319607c8aec884f4eae5143c
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-01-25 09:19:04 +0530

    Update admin-login.js

--- admin-login.js
+++ admin-login.js
@@ -1,9 +1,5 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "https://bmmtjsxwufevufovzkst.supabase.co";
-const supabaseKey = "sb_publishable_RiIZNtQDpXve8h6d1ajrFA_xienSBVl";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+// admin-login.js
+import { supabase } from "./supabase.js";
 
 window.login = async function () {
   const email = document.getElementById("emailInput").value.trim();
@@ -14,17 +10,15 @@ window.login = async function () {
     return;
   }
 
-  const { data, error } = await supabase.auth.signInWithPassword({
+  const { error } = await supabase.auth.signInWithPassword({
     email,
     password
   });
 
   if (error) {
-    console.error(error);
     alert(error.message);
     return;
   }
 
-  alert("Login successful!");
   window.location.href = "admin-dashboard.html";
 };
