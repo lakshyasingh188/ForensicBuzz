b6110099409d306275fced44e3308a67ee265457
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-01-25 20:36:11 +0530

    Update admin-dashboard.js

--- admin-dashboard.js
+++ admin-dashboard.js
@@ -5,3 +5,8 @@ const { data } = await supabase.auth.getUser();
 if (!data.user) {
   window.location.href = "admin-login.html";
 }
+
+window.logout = async function () {
+  await supabase.auth.signOut();
+  window.location.href = "admin-login.html";
+};
