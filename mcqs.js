6f26edfe60a4580cc0c714d0a74cb2a724fc6697
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-01-25 23:52:19 +0530

    Update mcqs.js

--- mcqs.js
+++ mcqs.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://bmmntjsxwufeuvfozkst.supabase.co";
+const supabaseUrl = "https://bmmmtjsxwufeuvfozkst.supabase.co";
 const supabaseKey = "sb_publishable_RiIZNtQDpXve8h6d1ajrFA_xienSBVl";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
