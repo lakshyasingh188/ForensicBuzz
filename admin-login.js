ece4af564edb4aca3f0c4733d7d06952e89090ce
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-01-25 15:33:53 +0530

    Update admin-login.js

--- admin-login.js
+++ admin-login.js
@@ -1,15 +1,20 @@
-import { supabase } from "./supabase.js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabase = createClient(
+  "https://bmmtjsxwufevufovzkst.supabase.co",
+  "sb_publishable_RiIZNtQDpXve8h6d1ajrFA_xienSBVl"
+);
 
 window.login = async function () {
   const email = document.getElementById("emailInput").value.trim();
   const password = document.getElementById("passwordInput").value.trim();
 
   if (!email || !password) {
-    alert("Email aur password required hai");
+    alert("Email aur password required");
     return;
   }
 
-  const { error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
