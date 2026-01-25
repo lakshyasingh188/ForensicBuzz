9847062eb9cff1916f1d86323dcc692c833f5e1a
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-01-25 09:18:23 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://bmmntjsxwufeuvfozkst.supabase.co",
+  "https://bmmtjsxwufevufovzkst.supabase.co",
   "sb_publishable_RiIZNtQDpXve8h6d1ajrFA_xienSBVl"
 );
