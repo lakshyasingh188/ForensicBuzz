9e68f517d3cd5983b101765eee308897f8d3c929
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-01-25 15:24:41 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from "https://unpkg.com/@supabase/supabase-js@2";
+// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-export const supabase = createClient(
-  "https://bmmtjsxwufevufovzkst.supabase.co",
-  "sb_publishable_RiIZNtQDpXve8h6d1ajrFA_xienSBVl"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
