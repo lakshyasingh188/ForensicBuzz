ae25cdad58c8fc8229d4cfde727c3715d6f87adb
Lakshya singh <lakshyasinghe.c.e@gmail.com>
2026-05-26 09:54:20 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,13 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const SUPABASE_URL =
+  "https://bmmmtjsxwufeuvfozkst.supabase.co";
 
-export const supabase = createClient(
-  "https://bmmtjsxwufeuvfozkst.supabase.co",  // ✅ Correct URL
-  "sb_publishable_RiIZNtQDpXve8h6d1ajrFA_xienSBV"
-);
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtbW10anN4d3VmZXV2Zm96a3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyNTYyODQsImV4cCI6MjA4NDgzMjI4NH0.btRW1CHoUbJodnXyvoUdji32dbwJW92mQOMXn7jVckM";
+
+window.supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
+
+console.log("Supabase Connected");
